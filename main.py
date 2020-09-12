6e093e24c19f2d44acacf57e987a3ae504d9f5cb
flaymax <69513102+flaymax@users.noreply.github.com>
2020-09-12 22:33:31 +0300

    first_commit_testing

--- main.py
+++ main.py
@@ -18,8 +18,19 @@ class Solution:
                         k += 1
 
 
+class sumi:
+    def factorial1(self, n):
+        return n * self.factorial1(n - 1) if n > 1 else 1
+    def sum_factorial(self, n):
+        summ=0
+        for i in range(n+1):
+            summ += self.factorial1(i)
+        return summ
+
 print(Solution().twoSum([int(input()) for i in range(3)], int(input())))
-print(np.exp(np.pi))
+print(sumi().sum_factorial(9))
+
+
 
 
 
