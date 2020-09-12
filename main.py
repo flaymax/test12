f6a22600cb85764a73428f2da1af65a5c750ccca
flaymax <69513102+flaymax@users.noreply.github.com>
2020-09-12 21:25:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Solution:
+    def twoSum(self, nums, target):
+        s = -1
+        k = 0
+        answer = []
+        for i in nums:
+            s += 1
+            k = 0
+            if len(answer) == 0:
+                for j in nums:
+                    if target == i + j and s != k:
+                        answer.append(s)
+                        answer.append(k)
+                        return answer
+                    else:
+                        k += 1
+
+
+print(Solution().twoSum([int(input()) for i in range(3)], 2))
+print(np.exp(np.pi))
+
+
+
